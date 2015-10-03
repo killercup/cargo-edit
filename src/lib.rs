1ac194ed21f2cae7466df0e8b5e372da3c60ae1d
Pascal Hertleif <killercup@gmail.com>
2015-10-03 16:48:37 +0200

    Add Imports for Tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,14 @@
 extern crate semver;
 extern crate toml;
 extern crate pad;
+#[cfg(test)] extern crate rustc_serialize;
 
 #[macro_use] mod utils;
 mod manifest;
 mod list;
 mod list_error;
 mod tree;
+#[cfg(test)] mod args;
 #[cfg(test)] mod manifest_test;
 
 pub use manifest::Manifest;
