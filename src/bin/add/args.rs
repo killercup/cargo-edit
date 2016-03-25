b11de395ae5541537d71f255d0ecf5b3a72aa39f
Marcello Seri <marcello.seri@gmail.com>
2016-03-25 13:15:35 +0000

    fix warning

--- src/bin/add/args.rs
+++ src/bin/add/args.rs
@@ -1,7 +1,6 @@
 ///! Handle `cargo add` arguments
 use semver;
 use std::error::Error;
-use std::path::Path;
 use cargo_edit::Dependency;
 use fetch::{get_crate_name_from_github, get_crate_name_from_gitlab, get_crate_name_from_path,
             get_latest_version};
