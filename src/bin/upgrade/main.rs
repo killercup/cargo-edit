318e5dfa444e20020b733603f3420e5d9e751821
Benjamin Gill <git@bgill.eu>
2017-12-23 10:17:09 +0000

    Failed attempt to get the test working...

--- src/bin/upgrade/main.rs
+++ src/bin/upgrade/main.rs
@@ -1,7 +1,7 @@
 //! `cargo upgrade`
 #![warn(missing_docs, missing_debug_implementations, missing_copy_implementations, trivial_casts,
-       trivial_numeric_casts, unsafe_code, unstable_features, unused_import_braces,
-       unused_qualifications)]
+        trivial_numeric_casts, unsafe_code, unstable_features, unused_import_braces,
+        unused_qualifications)]
 
 extern crate cargo_metadata;
 extern crate docopt;
