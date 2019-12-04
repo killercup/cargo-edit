ee4577bd8b3e4179eef691b5f31477d967b93e2a
oxalica <oxalicc@pm.me>
2019-12-04 15:47:35 +0000

    Avoid updating registry index in tests by default (#362)
    Currently, some tests fails on vendor-ed and locked registry, due to the requirement of updating it.
    
    This PR makes environment variable `CARGO_IS_TEST` used in tests to behave as argument `--offline` to avoid network access.
    Also, some tests invoking `cargo metadata`, which may fail on partial or outdated registry, are guarded under feature `test-external-apis`.

--- tests/cargo-upgrade.rs
+++ tests/cargo-upgrade.rs
@@ -385,6 +385,7 @@ fn upgrade_workspace() {
 
 /// Detect if attempting to run against a workspace root and give a helpful warning.
 #[test]
+#[cfg(feature = "test-external-apis")]
 fn detect_workspace() {
     let (_tmpdir, root_manifest, _workspace_manifests) = copy_workspace_test();
 
@@ -414,6 +415,7 @@ fn invalid_manifest() {
         "--manifest-path",
         &manifest,
     ])
+    .with_env(&[("CARGO_IS_TEST", "1")])
     .fails_with(1)
     .and()
     .stderr()
@@ -442,6 +444,7 @@ fn invalid_root_manifest() {
         "--manifest-path",
         &manifest,
     ])
+    .with_env(&[("CARGO_IS_TEST", "1")])
     .fails_with(1)
     .and()
     .stderr()
@@ -457,6 +460,7 @@ fn unknown_flags() {
         "foo",
         "--flag",
     ])
+    .with_env(&[("CARGO_IS_TEST", "1")])
     .fails_with(1)
     .and()
     .stderr()
@@ -473,6 +477,7 @@ For more information try --help ",
 
 // Verify that an upgraded Cargo.toml matches what we expect.
 #[test]
+#[cfg(feature = "test-external-apis")]
 fn upgrade_to_lockfile() {
     let (tmpdir, manifest) = clone_out_test("tests/fixtures/upgrade/Cargo.toml.lockfile_source");
     fs::copy(
@@ -489,6 +494,7 @@ fn upgrade_to_lockfile() {
 }
 
 #[test]
+#[cfg(feature = "test-external-apis")]
 fn upgrade_workspace_to_lockfile() {
     let (tmpdir, root_manifest, _workspace_manifests) = copy_workspace_test();
 
