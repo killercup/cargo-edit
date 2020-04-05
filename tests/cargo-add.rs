cb1b6aad0bc29d302764626f4ab81ec0cdc4d276
David Laban <alsuren@gmail.com>
2020-04-05 16:54:12 +0100

    fix unused variable that I introduced last week

--- tests/cargo-add.rs
+++ tests/cargo-add.rs
@@ -1036,8 +1036,6 @@ your-face = { version = "your-face--CURRENT_VERSION_TEST", features = ["mouth", 
 
 #[test]
 fn forbids_multiple_crates_with_features_option() {
-    let (_tmpdir, manifest) = clone_out_test("tests/fixtures/add/Cargo.toml.sample");
-
     assert_cli::Assert::command(&[
         get_command_path("add").as_str(),
         "add",
