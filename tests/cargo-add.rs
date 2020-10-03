ab22928ddfdeb0701c85eb3bf8bdebae4d976e28
Andronik Ordian <write@reusable.software>
2020-10-03 16:25:28 +0200

    Fix fetch for non lowercase crate names (#398)
    * tests: fix a warning
    
    * fetch: fix fetch for a non-lowercase crate name
    
    * tests: test a non-lowercase name

--- tests/cargo-add.rs
+++ tests/cargo-add.rs
@@ -1038,7 +1038,7 @@ your-face = { version = "your-face--CURRENT_VERSION_TEST", features = ["mouth", 
 
 #[test]
 fn forbids_multiple_crates_with_features_option() {
-    let (_tmpdir, manifest) = clone_out_test("tests/fixtures/add/Cargo.toml.sample");
+    let (_tmpdir, _manifest) = clone_out_test("tests/fixtures/add/Cargo.toml.sample");
 
     assert_cli::Assert::command(&[
         get_command_path("add").as_str(),
@@ -1084,6 +1084,7 @@ fn adds_dependency_normalized_name() {
         get_command_path("add").as_str(),
         "add",
         "linked_hash_map",
+        "Inflector",
         &format!("--manifest-path={}", manifest),
     ])
     .succeeds()
