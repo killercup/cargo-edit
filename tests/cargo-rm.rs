49b9e3f8c8a18ce2218460673b912a7a6871c06e
Squirrel <gilescope@gmail.com>
2021-09-19 22:43:06 +0100

    rm should drop associated features (#500)
    * rm should drop associated features
    
    (if last crate refernece)
    
    * Match `crate` as well as `crate/feature`

--- tests/cargo-rm.rs
+++ tests/cargo-rm.rs
@@ -14,6 +14,24 @@ fn remove_existing_dependency() {
     execute_command(&["rm", "docopt"], &manifest);
     let toml = get_toml(&manifest);
     assert!(toml["dependencies"]["docopt"].is_none());
+
+    // Activated features should not be changed:
+    assert_eq!(toml["features"]["std"].as_array().unwrap().len(), 2);
+}
+
+#[test]
+fn remove_existing_optional_dependency() {
+    let (_tmpdir, manifest) = clone_out_test("tests/fixtures/rm/Cargo.toml.sample");
+
+    let toml = get_toml(&manifest);
+    assert!(!toml["dependencies"]["clippy"].is_none());
+    assert_eq!(toml["features"]["annoy"].as_array().unwrap().len(), 1);
+
+    execute_command(&["rm", "clippy"], &manifest);
+    let toml = get_toml(&manifest);
+    assert!(toml["dependencies"]["clippy"].is_none());
+    // Also check that exact match feature activations are removed:
+    assert_eq!(toml["features"]["annoy"].as_array().unwrap().len(), 0);
 }
 
 #[test]
@@ -27,6 +45,15 @@ fn remove_multiple_existing_dependencies() {
     let toml = get_toml(&manifest);
     assert!(toml["dependencies"]["docopt"].is_none());
     assert!(toml["dependencies"]["semver"].is_none());
+
+    // "semver/std" activated feature should NOT have been dropped as
+    // there's still a build-dep on the crate:
+    assert_eq!(toml["features"]["std"].as_array().unwrap().len(), 2);
+
+    // Let's remove the last semver dependency and expect the associated feature to be dropped.
+    execute_command(&["rm", "--build", "semver"], &manifest);
+    let toml = get_toml(&manifest);
+    assert_eq!(toml["features"]["std"].as_array().unwrap().len(), 1);
 }
 
 #[test]
