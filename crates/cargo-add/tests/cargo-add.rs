70bffc6f196b84d98c478c7da301129f8238cd36
Ed Page <eopage@gmail.com>
2022-03-08 12:58:32 -0600

    refactor(tests): Rename cmd to be more general

--- crates/cargo-add/tests/cargo-add.rs
+++ crates/cargo-add/tests/cargo-add.rs
@@ -1,12 +1,12 @@
 #[test]
 fn cli_tests() {
     let t = trycmd::TestCases::new();
-    t.case("tests/cmd/add/*.toml");
+    t.case("tests/snapshots/add/*.toml");
     #[cfg(not(feature = "test-external-apis"))]
     {
-        t.skip("tests/cmd/add/invalid_git_external.toml");
-        t.skip("tests/cmd/add/invalid_name_external.toml");
-        t.skip("tests/cmd/add/add_normalized_name_external.toml");
-        t.skip("tests/cmd/add/git_external.toml");
+        t.skip("tests/snapshots/add/invalid_git_external.toml");
+        t.skip("tests/snapshots/add/invalid_name_external.toml");
+        t.skip("tests/snapshots/add/add_normalized_name_external.toml");
+        t.skip("tests/snapshots/add/git_external.toml");
     }
 }
