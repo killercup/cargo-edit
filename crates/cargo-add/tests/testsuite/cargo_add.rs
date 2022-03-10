91d8c96c57962af5b61759439fb9289459a9781f
Ed Page <eopage@gmail.com>
2022-03-10 12:11:00 -0600

    refactor: Organize files exactly like cargo

--- crates/cargo-add/tests/testsuite/cargo_add.rs
+++ crates/cargo-add/tests/testsuite/cargo_add.rs
@@ -1,11 +1,3 @@
-#![warn(rust_2018_idioms)]
-#![allow(clippy::all)]
-#![warn(clippy::needless_borrow)]
-#![warn(clippy::redundant_clone)]
-
-#[macro_use]
-extern crate cargo_test_macro;
-
 pub fn cargo_exe() -> &'static std::path::Path {
     snapbox::cmd::cargo_bin!("cargo-add")
 }
---
+++ crates/cargo-add/tests/testsuite/main.rs
@@ -0,0 +1,9 @@
+#![warn(rust_2018_idioms)]
+#![allow(clippy::all)]
+#![warn(clippy::needless_borrow)]
+#![warn(clippy::redundant_clone)]
+
+#[macro_use]
+extern crate cargo_test_macro;
+
+mod cargo_add;
