81a42599b0448b955c01362bfd3695a675d4a168
Ed Page <eopage@gmail.com>
2022-07-12 16:22:06 -0500

    Remove cargo-add
    I was originally planning on keeping this around for a while but in
    trying to sync `cargo-edit` lib code back from cargo will require
    significant changes to the `add` command and it just doesn't seem worht
    it.  Instead, we are letting people know what version they can install
    to access the old command.

--- src/lib.rs
+++ src/lib.rs
@@ -29,10 +29,7 @@ mod version;
 pub use crate_spec::CrateSpec;
 pub use dependency::Dependency;
 pub use errors::*;
-pub use fetch::{
-    get_features_from_registry, get_latest_dependency, get_manifest_from_path,
-    get_manifest_from_url, update_registry_index,
-};
+pub use fetch::{get_latest_dependency, update_registry_index};
 pub use manifest::{find, LocalManifest, Manifest};
 pub use metadata::{manifest_from_pkgid, workspace_members};
 pub use registry::registry_url;
