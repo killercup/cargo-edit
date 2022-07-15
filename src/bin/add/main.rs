2e5de7418037a11f8845e575f6f375b4ed57efd8
Ed Page <eopage@gmail.com>
2022-07-15 14:37:35 -0500

    fix: Improve the error when running cargo-add

---
+++ src/bin/add/main.rs
@@ -0,0 +1,29 @@
+//! `cargo add`
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications
+)]
+
+mod add;
+mod cli;
+
+use std::process;
+
+use clap::Parser;
+
+fn main() {
+    let args = cli::Command::parse();
+
+    if let Err(err) = args.exec() {
+        eprintln!("Error: {:?}", err);
+
+        process::exit(1);
+    }
+}
