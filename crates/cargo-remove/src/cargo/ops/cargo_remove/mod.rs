63ac3071d6e0a1e530dbecf240f8db0e196d7d1b
Cassaundra Smith <cass@cassaundra.org>
2022-08-30 14:01:32 -0700

    refactor(remove): use cargo internals to locate package manifest

--- crates/cargo-remove/src/cargo/ops/cargo_remove/mod.rs
+++ crates/cargo-remove/src/cargo/ops/cargo_remove/mod.rs
@@ -5,88 +5,60 @@ mod manifest;
 mod metadata;
 mod util;
 
-pub use dependency::Dependency;
-pub use dependency::PathSource;
-pub use dependency::RegistrySource;
-pub use dependency::Source;
-pub use manifest::{find, get_dep_version, set_dep_version, LocalManifest, Manifest};
+use cargo::core::Package;
+use cargo::CargoResult;
+
+pub use dependency::{Dependency, PathSource, RegistrySource, Source};
+pub use manifest::{
+    find, get_dep_version, set_dep_version, DepKind, DepTable, LocalManifest, Manifest,
+};
 pub use metadata::{manifest_from_pkgid, resolve_manifests, workspace_members};
 pub use util::{
     colorize_stderr, shell_note, shell_print, shell_status, shell_warn, shell_write_stderr, Color,
     ColorChoice,
 };
 
-use cargo::CargoResult;
-
-use std::borrow::Cow;
-use std::path::PathBuf;
-
 /// Remove a dependency from a Cargo.toml manifest file.
 #[derive(Debug)]
-pub struct RmOptions {
-    /// Dependencies to be removed
-    pub crates: Vec<String>,
-    /// Remove as development dependency
-    pub dev: bool,
-    /// Remove as build dependency
-    pub build: bool,
-    /// Remove as dependency from the given target platform
-    pub target: Option<String>,
-    /// Path to the manifest to remove a dependency from
-    pub manifest_path: Option<PathBuf>,
-    /// Package to remove from
-    pub pkg_id: Option<String>,
-    /// Don't actually write the manifest
+pub struct RmOptions<'a> {
+    /// Package to remove dependencies from
+    pub spec: &'a Package,
+    /// Dependencies to remove
+    pub dependencies: Vec<&'a String>,
+    /// Which dependency section to remove these from
+    pub section: DepTable,
+    /// Whether or not to actually write the manifest
     pub dry_run: bool,
     /// Do not print any output in case of success
     pub quiet: bool,
 }
 
-impl RmOptions {
-    /// Get dependency section
-    pub fn get_section(&self) -> Vec<String> {
-        let section_name = if self.dev {
-            "dev-dependencies"
-        } else if self.build {
-            "build-dependencies"
-        } else {
-            "dependencies"
-        };
-
-        if let Some(ref target) = self.target {
-            assert!(!target.is_empty(), "Target specification may not be empty");
-
-            vec!["target".to_owned(), target.clone(), section_name.to_owned()]
-        } else {
-            vec![section_name.to_owned()]
-        }
-    }
-}
-
 /// Remove dependencies from a manifest
-pub fn remove(args: &RmOptions) -> CargoResult<()> {
-    let manifest_path = if let Some(ref pkg_id) = args.pkg_id {
-        let pkg = manifest_from_pkgid(args.manifest_path.as_deref(), pkg_id)?;
-        Cow::Owned(Some(pkg.manifest_path.into_std_path_buf()))
-    } else {
-        Cow::Borrowed(&args.manifest_path)
-    };
-    let mut manifest = LocalManifest::find(manifest_path.as_deref())?;
-    let deps = &args.crates;
+pub fn remove(options: &RmOptions<'_>) -> CargoResult<()> {
+    let dep_table = options
+        .section
+        .to_table()
+        .into_iter()
+        .map(String::from)
+        .collect::<Vec<_>>();
 
-    deps.iter()
+    let manifest_path = options.spec.manifest_path().to_path_buf();
+    let mut manifest = LocalManifest::try_new(&manifest_path)?;
+
+    options
+        .dependencies
+        .iter()
         .map(|dep| {
-            if !args.quiet {
-                let section = args.get_section();
-                let section = if section.len() >= 3 {
-                    format!("{} for target `{}`", &section[2], &section[1])
+            if !options.quiet {
+                let section = if dep_table.len() >= 3 {
+                    format!("{} for target `{}`", &dep_table[2], &dep_table[1])
                 } else {
-                    section[0].clone()
+                    dep_table[0].clone()
                 };
                 shell_status("Removing", &format!("{dep} from {section}",))?;
             }
             let result = manifest
-                .remove_from_table(&args.get_section(), dep)
+                .remove_from_table(&dep_table, dep)
                 .map_err(Into::into);
 
             // Now that we have removed the crate, if that was the last reference to that crate,
@@ -97,7 +69,7 @@ pub fn remove(args: &RmOptions) -> CargoResult<()> {
         })
         .collect::<CargoResult<Vec<_>>>()?;
 
-    if args.dry_run {
+    if options.dry_run {
         shell_warn("aborting remove due to dry run")?;
     } else {
         manifest.write()?;
