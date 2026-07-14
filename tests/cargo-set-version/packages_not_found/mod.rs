bcc2c408018fac66545d7ae10b6e4c503318d66d
ychampion <ychampion@users.noreply.github.com>
2026-07-14 14:35:14 +0000

    test(set-version): expose unmatched selector no-ops

---
+++ tests/cargo-set-version/packages_not_found/mod.rs
@@ -0,0 +1,30 @@
+use cargo_test_support::Project;
+use cargo_test_support::compare::assert_ui;
+use cargo_test_support::current_dir;
+use cargo_test_support::file;
+use cargo_test_support::prelude::*;
+
+use crate::CargoCommand;
+
+#[cargo_test]
+fn case() {
+    let project = Project::from_template(current_dir!().join("in"));
+    let project_root = project.root();
+
+    snapbox::cmd::Command::cargo_ui()
+        .arg("set-version")
+        .args([
+            "2.0.0",
+            "--package",
+            "first-missing",
+            "--package",
+            "second-missing",
+        ])
+        .current_dir(&project_root)
+        .assert()
+        .code(0)
+        .stdout_eq(file!["stdout.term.svg"])
+        .stderr_eq(file!["stderr.term.svg"]);
+
+    assert_ui().subset_matches(current_dir!().join("out"), &project_root);
+}
